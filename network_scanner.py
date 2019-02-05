e50dbc6b63c0ad7b5c91655fb3238b89e5270f39
pbn <trev155@gmail.com>
2019-02-04 22:40:54 -0500

    Network Scanner

---
+++ network_scanner.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+import optparse
+
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+
+    # arp_request.show()
+    # broadcast.show()
+    # arp_request_broadcast.show()
+
+    # send and receive packets
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+
+    return clients_list
+
+
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n-----------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target", help="Target IP / IP range")
+    options, arguments = parser.parse_args()
+    if not options.target:
+        parser.error("[-] Please specify a target, use --help for more info")
+    return options
+
+
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
